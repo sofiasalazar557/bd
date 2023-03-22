b41837cdd44e09b0075f20ba32d666868d35f446
sofiasalazar557 <128562731+sofiasalazar557@users.noreply.github.com>
2023-03-21 21:30:32 -0500

    Create prueba.sql

---
+++ prueba.sql
@@ -0,0 +1,4 @@
+SELECT *
+FROM treintaco-bz.data_marts_pos.pos_logistic
+WHERE (tipo_envio = "LOCAL" and date(purchase_date) = date("2023-03-21") and extract(hour from purchase_date)<18) 
+or (tipo_envio = "NACIONAL" and date(delivery_date) = date("2023-03-22") )
