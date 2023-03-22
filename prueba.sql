90320a9eff20e4810db31a7b60fec13911a6164f
sofiasalazar557 <128562731+sofiasalazar557@users.noreply.github.com>
2023-03-21 21:56:54 -0500

    Update prueba.sql

--- prueba.sql
+++ prueba.sql
@@ -1,4 +1,4 @@
 SELECT *
 FROM treintaco-bz.data_marts_pos.pos_logistic
-WHERE (tipo_envio = "LOCAL" and date(purchase_date) = date("2023-03-21") and extract(hour from purchase_date)<18) 
-or (tipo_envio = "NACIONAL" and date(delivery_date) = date("2023-03-22") )
+WHERE  status_operator NOT IN("Entregado", "ENTREGADA POR TREINTA", "Servicio no creado") 
+and ( (tipo_envio = "LOCAL" and date(purchase_date) = date("2023-03-21") and extract(hour from purchase_date)<18) or (tipo_envio = "NACIONAL" and date(delivery_date) = date("2023-03-22")) )
